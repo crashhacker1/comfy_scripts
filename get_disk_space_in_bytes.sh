471760d159e9b6c2a326d146d499913f6f3b97b0
crashhacker <46489732+crashhack3r@users.noreply.github.com>
2021-12-23 22:20:28 +0530

    Delete get_disk_space_in_bytes.sh

--- get_disk_space_in_bytes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# get the remaining disk space in bytes
-
-find ./ -user cs16mtech11021 -type f -printf "%s\n" | awk '{t+=$1}END{print t}'
-
-du -smc /home/* | sort -n
-
-du -smc /media/* | sort -n
