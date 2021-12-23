160c7b71e7e305f164c3cc4bfd43f6b660a8d8a8
crashhacker <46489732+crashhack3r@users.noreply.github.com>
2021-12-23 22:20:38 +0530

    Delete get_media.sh

--- get_media.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# get all images from a chan board or any basic html page 
-
-exts=(webm mp4 jpeg txt c cpp m py h ppt pptx docx zip tar.gz gif png jpg pdf mp3 sh)
-tmpfile=".urls.tmp"
-lynx -dump -listonly -nonumbers $1 | sort -u > "$tmpfile"
-for ext in ${exts[*]}; do grep "\.$ext$" "$tmpfile" | tee -a $ext\_links.txt | aria2c -i - -d "$ext"; done
-rm "$tmpfile"
